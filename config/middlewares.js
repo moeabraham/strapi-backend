d69f45185074a8166235355c0b33e1413515196f
Mohamed Soliman <60075091+moeabraham@users.noreply.github.com>
2023-06-02 16:27:23 -0500

    strapi-provider upload do

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,6 +1,34 @@
 module.exports = [
   'strapi::errors',
-  'strapi::security',
+  // {
+    {
+      name: 'strapi::security',
+      config: {
+        contentSecurityPolicy: {
+          useDefaults: true,
+          directives: {
+            'connect-src': ["'self'", 'https:'],
+            'img-src': [
+              "'self'",
+              'data:',
+              'blob:',
+              'dl.airtable.com',
+              '*.digitaloceanspaces.com',
+            ],
+            'media-src': [
+              "'self'",
+              'data:',
+              'blob:',
+              'dl.airtable.com',
+              '*.digitaloceanspaces.com',
+            ],
+            upgradeInsecureRequests: null,
+          },
+        },
+      },
+    },
+  // }
+  // 'strapi::security',
   // 'strapi::cors',
   'strapi::poweredBy',
   {
@@ -8,7 +36,7 @@ module.exports = [
     config: {
       // enabled: true,
       // headers: '*',
-      origin: ['http://localhost:3000','http://localhost:1337', 'https://my-strapi-application-jqci7.ondigitalocean.app', 'http://moeonlinestore.com','https://moeonlinestore.com'],
+      origin: ['http://localhost:3000','http://localhost:1337', 'http://my-strapi-application-jqci7.ondigitalocean.app','https://my-strapi-application-jqci7.ondigitalocean.app', 'http://moeonlinestore.com','https://moeonlinestore.com'],
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
       headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
 
