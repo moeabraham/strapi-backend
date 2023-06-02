8fbe256f7e7d0b9a079bd2b9ca98a4333fe98cd1
Mohamed Soliman <60075091+moeabraham@users.noreply.github.com>
2023-06-02 00:12:08 -0500

    initial commit

---
+++ config/middlewares.js
@@ -0,0 +1,23 @@
+module.exports = [
+  'strapi::errors',
+  'strapi::security',
+  // 'strapi::cors',
+  'strapi::poweredBy',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: '*',
+      origin: ['http://localhost:3000','http://localhost:1337'],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      // headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+
+    }
+  },
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
